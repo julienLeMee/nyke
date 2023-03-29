9cbad7602faedc9ac28929327fb58c34f3fdb5c2
Julien Le Mee <julemee@gmail.com>
2023-03-29 16:16:27 -0400

    Navbar components

--- components/NavbarDown.jsx
+++ components/NavbarDown.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { BsBasket3 } from 'react-icons/bs'
 import { AiOutlineHeart } from 'react-icons/ai'
+import { CiSearch } from 'react-icons/ci'
 
 export const NavbarDown = () => {
   return (
     <>
-      <div className="flex items-center justify-between px-6">
-        <div className='flex items-center'>
-          <img src="nike-logo.png" alt="air jordan logo" width={60} height={60} />
+      <div className="flex items-center justify-between w-[95%] mx-auto py-1">
+        <div className='w-[80px]'>
+          <img src="nike-logo.png" alt="air jordan logo" width={80} height={80} />
         </div>
-        <div className='flex items-center'>
-          <ul className='flex'>
+        <div>
+          <ul className='flex ml-52'>
             <li className='ml-4 p-2'>New & Featured</li>
             <li className='ml-4 p-2'>Men</li>
             <li className='ml-4 p-2'>Women</li>
@@ -18,8 +19,20 @@ export const NavbarDown = () => {
           </ul>
         </div>
         <div className='flex items-center'>
-          <BsBasket3 size={20} />
-          <AiOutlineHeart className='ml-4' size={20} />
+          <div className='bg-gray-100 rounded-full hover:bg-black/10'>
+            <div className='flex items-center mr-3'>
+              <div className='p-2 rounded-full'>
+                <CiSearch />
+              </div>
+              <input placeholder='Search' type="text" className='border-none focus:outline-none bg-transparent rounded-full p-2 w-32' />
+            </div>
+          </div>
+          <div className="rounded-full cursor-pointer hover:bg-black/10 mx-2">
+            <AiOutlineHeart className='m-2' size={20} />
+          </div>
+          <div className="rounded-full cursor-pointer hover:bg-black/10 mx-2">
+            <BsBasket3 className='m-2' size={20} />
+          </div>
         </div>
       </div>
     </>
--- components/NavbarUp.jsx
+++ components/NavbarUp.jsx
@@ -3,20 +3,22 @@ import React from 'react'
 export const NavbarUp = () => {
   return (
     <>
-      <div className="flex items-center justify-between p-4">
+    <div className='bg-gray-100'>
+      <div className="flex items-center justify-between w-[95%] mx-auto py-1">
         <div className='flex items-center'>
           <img src="air-jordan-logo.png" alt="air jordan logo" width={30} height={30} />
           <img src="converse-logo.png" alt="air jordan logo" width={50} height={50} />
         </div>
         <div className='flex items-center'>
           <ul className='flex'>
-            <li className='text-center ml-4 px-4'>Find a store</li>
-            <li className='text-center ml-4 px-4 border-black border-l-2'>Help</li>
-            <li className='text-center ml-4 px-4 border-black border-l-2'>Join Us</li>
-            <li className='text-center ml-4 px-4 border-black border-l-2'>Sign In</li>
+            <li className='text-xs px-4 cursor-pointer hover:text-black/60'>Find a store</li>
+            <li className='text-xs px-4 cursor-pointer hover:text-black/60 border-black border-l-2'>Help</li>
+            <li className='text-xs px-4 cursor-pointer hover:text-black/60 border-black border-l-2'>Join Us</li>
+            <li className='text-xs px-4 cursor-pointer hover:text-black/60 border-black border-l-2'>Sign In</li>
           </ul>
         </div>
       </div>
+    </div>
     </>
   )
 }
