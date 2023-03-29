111e4e82fddc2a619fa9226d4dda799a37ff82c5
Julien Le Mee <julemee@gmail.com>
2023-03-29 11:55:53 -0400

    Navbar components

--- components/NavbarUp.jsx
+++ components/NavbarUp.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const NavbarUp = () => {
   return (
-    <div>Navbar up</div>
+    <div className="flex items-center justify-between p-4">
+    <div className='flex items-center'>
+      <img src="air-jordan-logo.png" alt="air jordan logo" width={30} height={30} />
+      <img src="converse-logo.png" alt="air jordan logo" width={50} height={50} />
+    </div>
+    <div className='flex items-center'>
+      <ul className='flex'>
+        <li className='ml-4 p-4'>Find a store</li>
+        <li className='ml-4 p-4 border-l-2'>Help</li>
+        <li className='ml-4 p-4 border-l-2'>Join Us</li>
+        <li className='ml-4 p-4 border-l-2'>Sign In</li>
+      </ul>
+    </div>
+    </div>
   )
 }
