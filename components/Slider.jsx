53c2298ead59aea5eccc37d078602fd21669885d
Julien Le Mee <julemee@gmail.com>
2023-03-29 23:25:52 -0400

    added sneakers img and name on slider

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -6,36 +6,44 @@ export const Slider = () => {
     <>
       <div className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
         <SliderItem
-          name='Air Max Pulse'
-          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/8318ac23-bd86-44fd-88f3-f21b435f41bb/nike-just-do-it.png'
-        />
-        <SliderItem
-          name='Air Max Furyosa'
-          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/b5c0fe85-935d-476a-aa65-a538e4c20311/nike-just-do-it.png'
-        />
-        <SliderItem
-          name='Air Max 90'
-          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/a4e31b52-6f35-485e-a085-7fa0fe4e3af4/nike-just-do-it.png'
-        />
-        <SliderItem
-          name='Air Max Plus'
-          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/9118bc53-fd8a-4dd3-beeb-4d5c31c7a67b/nike-just-do-it.png'
-        />
-        <SliderItem
-          name='Air Max 97'
-          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/0d1ec7bb-c77a-43d2-badd-2ff9da9ddc1c/nike-just-do-it.png'
-        />
-        <SliderItem
-          name='Air Max 95'
-          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/f0c8ffb7-6048-4204-9fd2-6da9426176a8/nike-just-do-it.png'
-        />
-        <SliderItem
+          position='7/8'
           name='Air Max 270'
           img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/14bc38c4-5979-4f80-b762-625d29375fd3/nike-just-do-it.png'
         />
         <SliderItem
+          position='8/8'
           name='Air Max TW'
           img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/5377c738-7cd6-4fe9-bb4c-ce9f41406cf8/nike-just-do-it.png'
+        />
+        <SliderItem
+          position='1/8'
+          name='Air Max Pulse'
+          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/8318ac23-bd86-44fd-88f3-f21b435f41bb/nike-just-do-it.png'
+        />
+        <SliderItem
+          position='2/8'
+          name='Air Max Furyosa'
+          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/b5c0fe85-935d-476a-aa65-a538e4c20311/nike-just-do-it.png'
+        />
+        <SliderItem
+          position='3/8'
+          name='Air Max 90'
+          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/a4e31b52-6f35-485e-a085-7fa0fe4e3af4/nike-just-do-it.png'
+        />
+        <SliderItem
+          position='4/8'
+          name='Air Max Plus'
+          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/9118bc53-fd8a-4dd3-beeb-4d5c31c7a67b/nike-just-do-it.png'
+        />
+        <SliderItem
+          position='5/8'
+          name='Air Max 97'
+          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/0d1ec7bb-c77a-43d2-badd-2ff9da9ddc1c/nike-just-do-it.png'
+        />
+        <SliderItem
+          position='6/8'
+          name='Air Max 95'
+          img='https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_300,c_limit/f0c8ffb7-6048-4204-9fd2-6da9426176a8/nike-just-do-it.png'
         />
       </div>
       {/* <div className="p-4">
--- components/SliderItem.jsx
+++ components/SliderItem.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export const SliderItem = ({ img, name}) => {
+export const SliderItem = ({ position, img, name}) => {
   return (
     <>
-      <div className='relative mx-auto h-auto w-auto duration-300 my-8 p-4 inline-block'>
+      <div className='relative mx-auto h-auto w-auto duration-300 my-8 p-4 inline-block group'>
         <Link href="/">
-          <Image className='h-auto w-auto object-cover' src={img} width={200} height={200} alt='sneakers' priority />
-          <p className='text-gray-500 tracking-wider py-4'>{name}</p>
+          <Image className='relative h-auto w-auto object-cover' src={img} width={200} height={200} alt='sneakers' priority />
+          <p className='opacity-0 group-hover:opacity-100 duration-300 absolute top-[15%] right-[20%] rounded-2xl text-white text-center font-light py-1 px-4 bg-[#797D80]'>{position}</p>
+          <p className='opacity-0 group-hover:opacity-100 duration-300 text-gray-500 tracking-wider py-4'>{name}</p>
         </Link>
       </div>
     </>
