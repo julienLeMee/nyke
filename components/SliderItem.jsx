995cacc80f4e53330ee71a07da237f193bab7bbd
Julien Le Mee <julemee@gmail.com>
2023-03-29 21:36:01 -0400

    added slider components

--- components/SliderItem.jsx
+++ components/SliderItem.jsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 export const SliderItem = ({ img, title}) => {
   return (
     <>
-      <div className='relative mx-auto h-auto w-auto duration-300 my-8'>
+      <div className='relative mx-auto h-auto w-auto duration-300 my-8 p-4'>
         <Link href="/">
-          <Image className='h-[300px] w-[300px]' src={img} width={200} height={200} alt='sneakers' style={{objectFit: 'cover'}} priority />
+          <Image className='h-auto w-auto' src={img} width={200} height={200} alt='sneakers' style={{objectFit: 'cover'}} priority />
         </Link>
         <p className='text-gray-500 tracking-wider py-4'>{title}</p>
       </div>
