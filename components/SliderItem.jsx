8e3f59086212c5478d804df1a495894b0835b91f
Julien Le Mee <julemee@gmail.com>
2023-03-29 23:04:46 -0400

    added sneakers img and name on slider

--- components/SliderItem.jsx
+++ components/SliderItem.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export const SliderItem = ({ img, title}) => {
+export const SliderItem = ({ img, name}) => {
   return (
     <>
       <div className='relative mx-auto h-auto w-auto duration-300 my-8 p-4 inline-block'>
         <Link href="/">
-          <Image className='h-auto w-auto' src={img} width={200} height={200} alt='sneakers' style={{objectFit: 'cover'}} priority />
-          <p className='text-gray-500 tracking-wider py-4'>{title}</p>
+          <Image className='h-auto w-auto object-cover' src={img} width={200} height={200} alt='sneakers' priority />
+          <p className='text-gray-500 tracking-wider py-4'>{name}</p>
         </Link>
       </div>
     </>
