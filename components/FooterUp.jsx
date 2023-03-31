81a41603b5eb3a1bc2aa629192766ca18febd4d1
Julien Le Mee <julemee@gmail.com>
2023-03-31 16:53:16 -0400

    added FooterDown components

--- components/FooterUp.jsx
+++ components/FooterUp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const FooterUp = () => {
   return (
     <>
-      <div className='w-full md:w-[60%] max-w-[95%] h-full mx-auto flex flex-col md:flex-row justify-around'>
+      <div className='w-full md:w-[60%] max-w-[95%] h-full mx-auto flex flex-col md:flex-row justify-around my-10'>
 
         <div className='flex flex-col'>
           <div className="p-2">
@@ -11,10 +11,10 @@ export const FooterUp = () => {
           </div>
           <div className="hidden md:block p-2">
             <ul className='sm:hidden md:block'>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Golf shoes</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Winter Trainers</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Gore Tex Trainers</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Walking trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Golf shoes</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Winter Trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Gore Tex Trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Walking trainers</li>
             </ul>
           </div>
         </div>
@@ -25,10 +25,10 @@ export const FooterUp = () => {
           </div>
           <div className="hidden md:block p-2">
             <ul className='sm:hidden md:block'>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Golf shoes</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Winter Trainers</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Gore Tex Trainers</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Walking trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Golf shoes</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Winter Trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Gore Tex Trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Walking trainers</li>
             </ul>
           </div>
         </div>
@@ -39,10 +39,10 @@ export const FooterUp = () => {
           </div>
           <div className="hidden md:block p-2">
             <ul className='sm:hidden md:block'>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Golf shoes</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Winter Trainers</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Gore Tex Trainers</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Walking trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Golf shoes</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Winter Trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Gore Tex Trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Walking trainers</li>
             </ul>
           </div>
         </div>
@@ -53,10 +53,10 @@ export const FooterUp = () => {
           </div>
           <div className="hidden md:block p-2">
             <ul className='sm:hidden md:block'>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Golf shoes</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Winter Trainers</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Gore Tex Trainers</li>
-              <li className='my-2 text-black/50 hover:text-black font-medium line-clamp-1'>Walking trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Golf shoes</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Winter Trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Gore Tex Trainers</li>
+              <li className='my-3 text-black/50 hover:text-black font-medium line-clamp-1'>Walking trainers</li>
             </ul>
           </div>
         </div>
