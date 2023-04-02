73de2ad7264afe543eede009977ff83baaae7f22
Julien Le Mee <julemee@gmail.com>
2023-04-02 12:35:43 -0400

    added dropdown component

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { DropdownItem } from './DropdownItem'
 
-export const Dropdown = () => {
+export const Dropdown = ({ li, title }) => {
   return (
-    <div>Dropdown</div>
+    <>
+      <div className='group duration-300'>
+        <p className='ml-4 p-2 border-transparent hover:border-black border-b-2 cursor-pointer duration-300'>{li}</p>
+        {/* Drop down */}
+          <div class="absolute w-screen top-15 left-[-3%] p-4 z-10 bg-white hidden group-hover:block text-black h-auto">
+            <p className='mb-4 font-medium px-4'>{title}</p>
+            <DropdownItem link="/" name="Portfolio" />
+            <DropdownItem link="/" name="Linkdedin" />
+            <DropdownItem link="/" name="Github" />
+            <DropdownItem link="/" name="Twitter" />
+          </div>
+      </div>
+    </>
   )
 }
--- components/DropdownItem.jsx
+++ components/DropdownItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const DropdownItem = () => {
+export const DropdownItem = ({ link, name }) => {
   return (
-    <div>DropdownItem</div>
+    <>
+      <a href={link} class="block text-sm py-2 px-4 text-black/50 hover:text-black duration-200">{name}</a>
+    </>
   )
 }
